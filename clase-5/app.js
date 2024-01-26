26750d0a4f307cfd8dc34177f8b783b000b74d6a
Felipe Andres Lopez <felipe.lopezmoreno@baptisthealth.net>
2024-01-26 15:47:38 -0500

    add dependencies injection

--- clase-5/app.js
+++ clase-5/app.js
@@ -1,16 +1,18 @@
 import express, { json } from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
+import { createMovieRouter } from './routes/movies.js'
 
-import { moviesRouter } from './routes/movies.js'
 const PORT = process.env.PORT ?? 3000
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware())
-app.disable('x-powered-by')
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
+  app.use(corsMiddleware())
+  app.disable('x-powered-by')
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT} on port http://localhost:${PORT}`)
+  })
+}
